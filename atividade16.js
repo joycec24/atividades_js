2f97cf979a143317f39143a47146f5cdfe6e547d
Joyce de Carvalho <joyce.c.silva8@aluno.senai.br>
2025-06-26 16:18:28 -0300

    atividade16

---
+++ atividade16.js
@@ -0,0 +1,5 @@
+let frutas = ['maçã','banana','laranja','melancia','framboesa']
+let first = frutas[0]
+let last = frutas[frutas.length-1]
+
+console.log("Primeiro item: ",first,"\nÚltimo item: ",last)
