83746a6f1f0fedd4556f3119921e7a5148c6ab92
Joyce de Carvalho <joyce.c.silva8@aluno.senai.br>
2025-06-26 15:44:49 -0300

    atividade6

--- atividade5.js
+++ atividade5.js
@@ -2,4 +2,4 @@ let n1 = 32
 let n2 = 59
 let soma = n1+n2
 
-console.log(soma)
+console.log(n1,"+",n2," = ",soma)
---
+++ atividade6.js
@@ -0,0 +1,5 @@
+let n1 = 17
+let n2 = 4
+let sub = n1-n2
+
+console.log(n1,"-",n2," = ",sub)
